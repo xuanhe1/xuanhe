3b177742732d6587dbed1aeadb03ac7e3b618ad3
wpengfei1234 <1341913464@qq.com>
2019-08-14 09:03:17 +0800

    123

---
+++ js/wang/wheel.js
@@ -0,0 +1,70 @@
+var ul = document.querySelector('.wrap ul');
+var div = document.querySelector('.second');
+var prev = document.querySelector('.prev');
+var next = document.querySelector('.next');
+var index = 0;
+var then = new Date();
+
+//初始化
+$('.fixed li').eq(index).css({
+	border: '1px solid orange'
+})
+$('.fixed li').eq(index).find('p').css({
+	background: 'orange'
+})
+
+//点击楼层按钮
+$(".fixed li").click(function(){
+	var count = $(this).index();
+	var iTop = -100 * count + "%"
+	$(".wrap ul").css({top:iTop},1000);
+	index = count;
+	//调用公共的侧边栏js
+	common();
+})
+//封装侧边栏公共的js
+function common(){
+	$('.fixed li').eq(index).css({
+		border: '1px solid orange'
+	}).siblings().css({
+		border: ''
+	})
+	$('.fixed li').eq(index).find('p').css({
+		background: 'orange'
+	}).parent().siblings().find('p').css({
+		background: ''
+	})
+	$('.fixed_content .num p').eq(0).find('span').html(index+1);
+}
+
+function wheel(e) {
+	var now = new Date();
+	var e = e || window.event;
+	if (now - then > 500) {
+		if (e.detail) {
+			if (e.detail > 0) {
+				index++;
+			} else {
+				index--;
+			}
+		} else {
+			if (e.wheelDelta > 0) {
+				index--;
+			} else {
+				index++;
+			}
+		}
+		if (index < 0) {
+			index = 0;
+		}else if(index>4){
+			index = 4;
+		}
+		//调用公共的侧边栏js
+		common()
+		//滚轮事件改变ul的top值
+		ul.style.top = -100 * index + "%"
+		then = new Date();
+	}
+}
+document.addEventListener('DOMMouseScroll', wheel, false);
+document.onmousewheel = wheel;
